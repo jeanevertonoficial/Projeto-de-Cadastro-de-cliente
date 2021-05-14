2cfd64da7713401109c9a50a07f6d876308f3dfe
jeanevertonoficial <jeanever39@gmail.com>
2021-05-14 12:53:11 -0300

    Troca de titilo no arquivo form_cadstrar.php

--- form_cadastrar.php
+++ form_cadastrar.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bootstrap.min.css">
-    <title>Curso de criação de formulario</title>
+    <title>Cadastre-se</title>
 </head>
 <body>
 
