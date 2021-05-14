c681b88b73d9696fc51896e364d3c3ecc4768711
Jean de Jesus <jean@SERRANO.LOCAL>
2021-05-14 16:48:03 -0300

    Alteração no titulo da pagina, por Roberta

--- cadastro.php
+++ cadastro.php
@@ -14,7 +14,7 @@ $contato = new \App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['c
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bootstrap.min.css">
-    <title>Curso Strings</title>
+    <title>Autoload pag</title>
 </head>
 <body>
 
